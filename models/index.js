6016534106eb6b7e629fa4dbaf5313fa823fd6d3
tniemeye19 <timothy.niemeyer19@gmail.com>
2021-11-02 16:16:58 -0400

    created and defined the model associations

--- models/index.js
+++ models/index.js
@@ -1,4 +1,13 @@
 const User = require('./User');
 const Post = require('./Post');
 
+// Create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
 module.exports = { User, Post };
