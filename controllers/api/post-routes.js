e41d8ce95ebc94f802f82841c23c0888244ea8f7
tniemeye19 <timothy.niemeyer19@gmail.com>
2021-11-09 21:10:34 -0500

    created a single post template. Added upvote functionality. Added comment functionality. Conditionally rendered the form elements. Conditionally rendered the login links

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -95,13 +95,16 @@ router.post('/', (req, res) => {
 
 // PUT / Create the Vote
 router.put('/upvote', (req, res) => {
-    // Custom static method created in models/Post.js
-    Post.upvote(req.body, { Vote })
-    .then(updatedPostData => res.json(updatedPostData))
-    .catch(err => {
-        console.log(err);
-        res.status(400).json(err);
-    });
+    // make sure the session exists first
+    if (req.session) {
+        // pass session id along with all destructured properties on req.body
+        Post.upvote({...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+        .then(updatedVotetData => res.json(updatedVoteData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    }
 });
 
 // Update a Posts title
